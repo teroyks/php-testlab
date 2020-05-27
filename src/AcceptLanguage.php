ff6854c26be4e32e766f20a4a0ade0896698d728
Tero Ykspetäjä <tero@tero.dev>
2020-05-27 09:01:13 +0000

    ✨ Support selecting country only from given values
    If optional parameter is given, ignores all country codes not included.
    
    Rename partial component parsing methods; add method documentation.
    Add test cases for outlier values.

--- src/AcceptLanguage.php
+++ src/AcceptLanguage.php
@@ -6,15 +6,23 @@
 /**
  * Parses the country code from Accept-Language.
  * 
+ * If the value contains multiple codes, chooses the one with the highest priority.
+ * 
+ * @param string $value Accept-Language value
+ * @param array $availableLanguages Choose only from these languages (default = all)
+ * 
  * @return Two-letter country code (lowercase), or null if not found.
  */
-function getCountryCode(string $value): ?string
+function getCountryCode(string $value, array $availableLanguages = []): ?string
 {
     $valuesWithPreference = [];
     foreach (explode(',', $value) as $item) {
         $parsed = parseSingleItem(trim($item));
         if (!$parsed) continue;
+
         list($code, $preference) = $parsed;
+        if ($availableLanguages && !in_array($code, $availableLanguages)) continue;
+
         $valuesWithPreference[$code] = $preference;
     }
 
@@ -23,19 +31,36 @@ function getCountryCode(string $value): ?string
     return $valuesWithPreference ? array_keys($valuesWithPreference)[0] : null;
 }
 
+/**
+ * Parses a single Accept-Language item.
+ * 
+ * @param string $item Language code and optional priority: code|code;q=priority
+ * 
+ * @return array|null [country code, priority], or null if no country code found
+ */
 function parseSingleItem(string $item): ?array
 {
     $defaultPriority = 1;
 
     preg_match('/^([^;]*)(?:;q=([\d.]+))?$/', trim($item), $matches);
-    $code = getCountryFromCode($matches[1]);
+    $code = parseCountryFromCode($matches[1]);
 
     return $code
         ? [$code, $matches[2] ?? $defaultPriority]
         : null;
 }
 
-function getCountryFromCode(string $value): ?string
+/**
+ * Tries to parse a country code from the language code.
+ * 
+ * If the code is in the format 'll-cc', it is assumed that 'cc' is a two-letter
+ * country code.
+ * 
+ * @param string $value Language code
+ * 
+ * @return string|null Country code, or null if not found.
+ */
+function parseCountryFromCode(string $value): ?string
 {
     if (preg_match('/^\w\w-(\w\w)$/', $value, $matches))
         return strtolower($matches[1]);
--- tests/unit/AcceptLanguageTest.php
+++ tests/unit/AcceptLanguageTest.php
@@ -8,19 +8,20 @@ class AcceptLanguageTest extends TestCase
 {
     function testEmptyValue()
     {
-        $this->assertNull(getCountryFromCode(''));
+        $this->assertNull(parseCountryFromCode(''));
         $this->assertNull(parseSingleItem(''));
     }
 
     function testOnlyLanguageCode()
     {
-        $this->assertNull(getCountryFromCode('fi'));
+        $this->assertNull(parseCountryFromCode('fi'));
         $this->assertNull(parseSingleItem('fi'));
+        $this->assertNull(parseCountryFromCode('*'));
     }
 
     function testLanguageAndCountryCode()
     {
-        $this->assertEquals('fi', getCountryFromCode('sv-FI'));
+        $this->assertEquals('fi', parseCountryFromCode('sv-FI'));
     }
 
     function testSingleItemWithDefaultPriority()
@@ -38,11 +39,28 @@ class AcceptLanguageTest extends TestCase
         $this->assertNull(parseSingleItem('fi;q=0.9'));
     }
 
+    function testSingleCountryCode()
+    {
+        $this->assertEquals('fi', getCountryCode('fi-FI'));
+    }
+
     function testPreferredCode()
     {
         $this->assertEquals('ch', getCountryCode('fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'),
             'Should return country code from highest (default) value fr-CH.');
         $this->assertEquals('se', getCountryCode('ch, fi-FI;q=0.8, sv-SE;q=0.9, lt-LT;q=0.7'),
             'Should return country code with highest priority.');
+    }
+
+    function testAcceptedCodes()
+    {
+        $this->assertEquals('fi',
+            getCountryCode('fr-ch;q=0.9, fi-FI;q=0.8, sv-SE;q=0.7', ['fi', 'se']),
+            'Should exclude CH even though it has the highest priority.'
+        );
+        $this->assertEquals('se',
+            getCountryCode('fr-ch;q=1.0, fi-FI;q=0.8, sv-SE;q=0.85', ['fi', 'se']),
+            'Should pick allowed code with highest priority.'
+        );
     }
 }
